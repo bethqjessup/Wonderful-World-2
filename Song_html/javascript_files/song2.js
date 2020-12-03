00fffaef76944f45aaf24e023029fee71493cf5b
bethqjessup <74424330+bethqjessup@users.noreply.github.com>
2020-12-03 15:42:56 -0500

    dec3_update

--- Song_html/javascript_files/song2.js
+++ Song_html/javascript_files/song2.js
@@ -1,2 +1,40 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
+const name="Beth";
+Const youtuber="traversy Media"
+
+Function toupper(text){
+const upperCase=text.toUpperCase();
+console.log(upperCase);
+
+} 
+
+
+
+toUpper(name);
+toUpper(youtuber)
+
+// example code from a tutorial above 
+
+/*
+var lastScrollTop=0;
+$(document).scolll(function(){
+	var scrollTop=$(document).scrollTop();
+	if (scrollTop>lastScrollTop){
+		console.log("scrolling down")
+	}
+	else if (scrollingTop<lastScrollTop){
+		console.log("scrolling up")
+	}
+	lastScrollTop=scrollTop
+});
+
+function myFunction() {
+  var x = document.getElementById('myDIV');
+  if (x.style.visibility === 'hidden') {
+    x.style.visibility = 'visible';
+  } else {
+    x.style.visibility = 'hidden';
+  }
+}
+
+*/ 
